2a7e297dd3e1db32d09b682779da6adfb39241d6
Serge Mosesov <smosesov@gmail.com>
2023-05-21 13:58:42 +0200

    black

--- mrkl_agent.py
+++ mrkl_agent.py
@@ -1,5 +1,5 @@
 """
-Using sly and sly_llama to implement MRLKL
+Using sly and sly_llama to implement MRKL
 """
 
 from typing import List, Optional
@@ -11,8 +11,8 @@ from sly_llama import llm_call, SlyBaseModel, LlmException
 from langchain import OpenAI
 from langchain.agents import Tool
 
-#TODO set model name in .env
-llm = OpenAI(model_name='gpt-3.5-turbo')
+# TODO set model name in .env
+llm = OpenAI(model_name="gpt-3.5-turbo")
 
 
 class MrklLexer(Lexer):
@@ -22,12 +22,11 @@ class MrklLexer(Lexer):
     pydantic class.
     """
 
-    #define the tokens we are going to match
+    # define the tokens we are going to match
     tokens = {ACTION_INPUT, ACTION, THOUGHT, FINAL_ANSWER}
 
-    #ignore ne lines so that we can match multiline tokens
+    # ignore new lines so that we can match multiline tokens
     ignore = "\t \n"
-
 
     # define regex for each token and clean the captured string value in token.value
 
@@ -38,9 +37,7 @@ class MrklLexer(Lexer):
 
     @_(r"Action Input(.|\n)*?(?=Observation)")
     def ACTION_INPUT(self, token):
-        token.value = (
-                token.value.replace("Action Input:", "")
-        )
+        token.value = token.value.replace("Action Input:", "")
         return token
 
     @_(r"Action(.|\n)*(?=Action Input)")
@@ -50,9 +47,7 @@ class MrklLexer(Lexer):
 
     @_(r"Final Answer(.|\n)*")
     def FINAL_ANSWER(self, token):
-        token.value = (
-            token.value.replace("Final Answer", "").strip("\n ")
-        )
+        token.value = token.value.replace("Final Answer", "").strip("\n ")
 
         return token
 
@@ -60,16 +55,15 @@ class MrklLexer(Lexer):
     ignore_rest = ".+"
 
 
-
 class MrklOutput(SlyBaseModel):
     """
     Model to validate the output of the Mrkl llm call
     """
+
     action: Optional[str] = None
     action_input: Optional[str] = None
     thought: Optional[str] = None
     final_answer: Optional[str] = None
-
 
     @staticmethod
     def lexer():
@@ -79,7 +73,6 @@ class MrklOutput(SlyBaseModel):
         more info.
         """
         return MrklLexer()
-
 
     @root_validator(pre=True)
     def check_action_or_answer(cls, values):
@@ -97,7 +90,13 @@ class MrklOutput(SlyBaseModel):
         return values
 
 
-@llm_call(llm, stop_sequence="Observation", verbose=False, return_prompt=True, return_llm_output=True)
+@llm_call(
+    llm,
+    stop_sequence="Observation",
+    verbose=False,
+    return_prompt=True,
+    return_llm_output=True,
+)
 def mrkl_start(tools, tool_names, request) -> MrklOutput:
     """
     You are a helpful assistant designed to answer quesitons.
@@ -128,21 +127,29 @@ def mrkl_start(tools, tool_names, request) -> MrklOutput:
     """
 
 
-@llm_call(llm, stop_sequence="Observation", verbose=False, return_prompt=True, return_llm_output=True)
+@llm_call(
+    llm,
+    stop_sequence="Observation",
+    verbose=False,
+    return_prompt=True,
+    return_llm_output=True,
+)
 def mrkl_step(history, current_observation) -> MrklOutput:
     """
     {history}{current_observation}
     """
 
 
-def insert_newline_after_match(string, pattern: str = 'Action Input:'):
+def insert_newline_after_match(string, pattern: str = "Action Input:"):
     """
     Inserts a newline after the given pattern in the given string.
     """
-    return string.replace(pattern, pattern + '\n')
+    return string.replace(pattern, pattern + "\n")
 
 
-def mrlkl_agent(query: str, tools_list: List[Tool], max_iters: int, max_retries: int) -> str | None:
+def mrlkl_agent(
+    query: str, tools_list: List[Tool], max_iters: int, max_retries: int
+) -> str | None:
     """
     Runs the MRLKL agent with the given query, tools list, and maximum number of iterations.
 
@@ -159,42 +166,44 @@ def mrlkl_agent(query: str, tools_list: List[Tool], max_iters: int, max_retries:
     """
 
     tools = {tool.name: tool for tool in tools_list}
-    tool_info = str(({t.name : t.description for t in tools.values()}))
+    tool_info = str(({t.name: t.description for t in tools.values()}))
     tool_names = str(tools.keys())
 
     # Start the MRLKL agent with the initial conditions
     mrlkl_output, first_prompt, raw_output = mrkl_start(tool_info, tool_names, query)
 
-    last_output = insert_newline_after_match(raw_output, 'Action Input:')
+    last_output = insert_newline_after_match(raw_output, "Action Input:")
     history = first_prompt + last_output
 
     print(history)
 
     for _ in range(max_iters):
-
         if mrlkl_output.action in tools:
             current_observation = tools[mrlkl_output.action](mrlkl_output.action_input)
         else:
-            current_observation = f'{mrkl_start.action} not a valid tool, try another one'
+            current_observation = (
+                f"{mrkl_start.action} not a valid tool, try another one"
+            )
 
         for i in range(max_retries):
-
             try:
-                mrlkl_output, last_prompt, raw_output = mrkl_step(history, current_observation)
+                mrlkl_output, last_prompt, raw_output = mrkl_step(
+                    history, current_observation
+                )
                 break
 
             except LlmException as e:
                 current_observation = e.message
-                mrlkl_output, last_prompt, raw_output = mrkl_step(history, current_observation)
+                mrlkl_output, last_prompt, raw_output = mrkl_step(
+                    history, current_observation
+                )
 
         # the llm one shot learns better if it can see last action separated by new line, esp code indent
-        last_output = insert_newline_after_match(raw_output, 'Action Input:')
+        last_output = insert_newline_after_match(raw_output, "Action Input:")
 
         history = last_prompt + last_output
         print(last_prompt)
         print(last_output)
 
-
         if mrlkl_output.final_answer:
             return mrlkl_output.final_answer
-
