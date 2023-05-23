e506d3a9c5b8bee1325ab7e3b4786823a74d0ff2
Serge Mosesov <smosesov@gmail.com>
2023-05-23 22:33:39 +0100

    black"

--- sly_llama/models.py
+++ sly_llama/models.py
@@ -8,50 +8,50 @@ import re
 
 
 class PatternError(Exception):
-    '''
+    """
     Exception raised if there's some kind of problem with the specified
     regex patterns in the lexer.
-    '''
+    """
+
     pass
 
 
 class MetaLLmOutput(ModelMetaclass):
-
     @classmethod
     def __prepare__(mcs, name, bases, **kwargs):
-
-
         namespace = OrderedDict()
 
         def _(pattern, *extra):
             patterns = [pattern, *extra]
+
             def decorate(func):
-                pattern = '|'.join(f'({pat})' for pat in patterns )
-                if hasattr(func, 'pattern'):
-                    func.pattern = pattern + '|' + func.pattern
+                pattern = "|".join(f"({pat})" for pat in patterns)
+                if hasattr(func, "pattern"):
+                    func.pattern = pattern + "|" + func.pattern
                 else:
                     func.pattern = pattern
                 return func
+
             return decorate
 
-        namespace['_'] = _
+        namespace["_"] = _
 
         return namespace
 
     def __new__(mcs, name, bases, namespace, **kwargs):
-
-        del namespace['_']
+        del namespace["_"]
 
         # construct the namespace for pydanticn __new__ excluding the parsing funcs
         new_namespace = OrderedDict()
         parse_funcs = {}
+        # extract the parsing functions to dict
         for key, value in namespace.items():
-            if hasattr(value, 'pattern'):
+            if hasattr(value, "pattern"):
                 parse_funcs[key] = value
             else:
                 new_namespace[key] = value
-        # extract the parsing functions to dict
-        #TODO validate parse func names against namespace
+
+        # TODO validate parse func names against namespace
         # get cls from pydantic __new__
         cls = super().__new__(mcs, name, bases, new_namespace, **kwargs)
 
@@ -61,14 +61,9 @@ class MetaLLmOutput(ModelMetaclass):
         return cls
 
 
-
-
 class LlmOutput(BaseModel, metaclass=MetaLLmOutput):
-
-
     @classmethod
     def from_llm_output(cls, llm_output: str):
-
         # construct the dict from wich to initilise the pydantic object
         args_dict = {}
 
@@ -77,14 +72,14 @@ class LlmOutput(BaseModel, metaclass=MetaLLmOutput):
             pattern = parse_func.pattern
 
             try:
-                #TODO define re flags
+                # TODO define re flags
                 cpat = re.compile(pattern)
             except Exception as e:
-                raise PatternError(f'Invalid regex for field {field_name}') from e
+                raise PatternError(f"Invalid regex for field {field_name}") from e
 
             # Verify that the pattern doesn't match the empty string
-            if cpat.match(''):
-                raise PatternError(f'Regex for field {field_name} matches empty input')
+            if cpat.match(""):
+                raise PatternError(f"Regex for field {field_name} matches empty input")
 
             re_matches = cpat.findall(llm_output)
 
